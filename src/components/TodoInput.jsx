e25dca18faaeb94487ca6574561ef6ab40528379
Abenezer <abitysank@gmail.com>
2025-12-22 16:24:20 -0500

    Add TodoInput component with basic form
    Introduces a new TodoInput component using useState for input management. The form currently handles input changes and prevents empty submissions, but does not yet process or store todos.

---
+++ src/components/TodoInput.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function TodoInput() {
+    const [text, setText] = useState("");
+
+    const handleSumbit = (e) => {
+        e.preventDefault();
+        if (!text.trim()) return;
+
+        setText;
+    };
+    return (
+        <form onSubmit={handleSumbit}>
+            <input
+                type="text"
+                onChange={(e) => setText(e.target.value)}
+                placeholder="Add a todo"
+            />
+            <button type="submit"></button>
+        </form>
+    );
+}

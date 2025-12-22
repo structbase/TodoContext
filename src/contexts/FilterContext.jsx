57db7b7fb278cbb2bf40dcd72a532037c79e4f51
Abenezer <abitysank@gmail.com>
2025-12-22 15:18:17 -0500

    Implement FilterProvider and useFilter hook
    Added stateful FilterProvider to manage filter state and a useFilter hook for consuming the context. Ensures filter context is only used within the provider.

--- src/contexts/FilterContext.jsx
+++ src/contexts/FilterContext.jsx
@@ -2,4 +2,21 @@ import { createContext, useContext, useState } from "react";
 
 const FilterContext = createContext(null);
 
-export function FilterProvider() {}
+export function FilterProvider({ children }) {
+    const [filter, setFilter] = useState("all");
+
+    return (
+        <FilterContext.Provider value={{ filter, setFilter }}>
+            {children}
+        </FilterContext.Provider>
+    );
+}
+
+export function useFilter() {
+    const context = useContext(FilterContext);
+
+    if (!context) {
+        throw new Error("useFilter must be used inside TodoProvider");
+    }
+    return context;
+}
